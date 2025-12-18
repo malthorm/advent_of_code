bb2fa3569067013bc108164c94474e93fc7b59cd
Malte Thormählen <malte.thormaehlen@vivavis.com>
2025-12-18 15:58:38 +0100

    complete day7 part 2

--- 2025/day7.py
+++ 2025/day7.py
@@ -16,40 +16,46 @@ input = """.......S.......
 ..............."""
 
 
-# with open("./day7_input.txt") as infile:
-#     input = infile.read()
+with open("./day7_input.txt") as infile:
+    input = infile.read()
 
-splitters = []
+splitters = set()
 
 timelines = 0
 input_arr = input.split("\n")
 beams = set()
 memo = {}
+start = None
+total = 0
 for i, char in enumerate(input_arr[0]):
     if char == "S":
         beams.add((0, i))
-
-for i, line in enumerate(input_arr[:-1]):
-    # beams_on_line = beams[i]
-    for j, char in enumerate(line):
-        if char == "^" and (i - 1, j) in beams and (j - 1 > 0 or j + 1 < len(line)):
-            beams.add((i + 1, j - 1))
-            beams.add((i + 1, j + 1))
-            beams.remove((i - 1, j))
-            # total_splits += 1
-        elif char == "." and (i - 1, j) in beams:
-            beams.add((i, j))
+        start = (0, i)
+for i, line in enumerate(input_arr):
+    for j, char in enumerate(list(line)):
+        if char == "^":
+            splitters.add((i, j))
 
 
-def count_timelines(start, beams):
-    if start in memo:
-        return memo[start]
-    line_idx = start[0]
-    if line_idx == len(input_arr) - 1:
+left_right = []
+
+
+def count_timelines(beam):
+    x, y = beam
+    if (x + 1, y) in memo:
+        return memo[(x + 1, y)]
+    if x == len(input_arr) - 1:
         return 1
-    for j, char in enumerate(input_arr[line_idx]):
-        pass
+    if (x + 1, y) in splitters:
+        left = count_timelines((x + 1, y - 1))
+        right = count_timelines((x + 1, y + 1))
+        memo[(x + 1, y)] = left + right
+        return left + right
+    else:
+        return count_timelines((x + 1, y))
 
+
+print(count_timelines(start))
 
 # # part1
 # splitters = []
